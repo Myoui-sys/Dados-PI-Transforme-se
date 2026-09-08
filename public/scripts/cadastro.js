177b62a8fa85e60a812e98c2c384cd63429512c5
Myoui-sys <meninagamer55@gmail.com>
2026-09-08 18:52:29 -0300

    Salva alteração local em cadastro.js

--- public/scripts/cadastro.js
+++ public/scripts/cadastro.js
@@ -17,7 +17,7 @@ document.querySelector("form").addEventListener("submit", async (event) => {
         body: JSON.stringify(dados)
     });
 
-    // Converte a resposta do servidor para JSON
+    // Converte a resposta dgit diff feature-testes_automatizadosclico2..origin/maino servidor para JSON
     const resultado = await response.json();
 
     // Seleciona o elemento que exibirá mensagens de feedback para o usuário
